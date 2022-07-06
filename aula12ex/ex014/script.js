5567673efe6cdb3286d8e696755d56a36d2d0b4b
Kaique Goncalves <104577183+kafogaca@users.noreply.github.com>
2022-07-06 18:22:08 -0300

    exercicio 01 aula 14

---
+++ aula12ex/ex014/script.js
@@ -0,0 +1,23 @@
+function carregar() {
+    var msg = document.getElementById('msg')
+    var img = document.getElementById('imagem')
+    var data = new Date()
+    var hora = data.toLocaleTimeString() /* Tras a hora completa do sistema para uma string */
+    window.setInterval('carregar()', 1000) /* Faz a pagina carregar sem precisar ficar atualizando */
+
+    msg.innerHTML = `${hora}`
+
+    if (hora >= 0 && hora < 12) {
+        img.src = '../imagens/manha.png'
+        document.body.style.background = '#e2cd9f'
+        frase.innerHTML = 'Tenha uma ótima Manhã'
+    } else if (hora >= 12 && hora <= 18) {
+        img.src = '../imagens/tarde.png'
+        document.body.style.background = '#b9846f'
+        frase.innerHTML = 'Tenha uma ótima Tarde'
+    } else {
+        img.src = '../imagens/noite.png'
+        document.body.style.background = '#0c0c2c'
+        frase.innerHTML = 'Tenha uma ótima Noite'
+    }
+}
